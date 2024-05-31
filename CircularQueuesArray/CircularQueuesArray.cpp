14981490179f2cea9285708179a83cb78431f8c8
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-31 11:16:30 +0700

    jika elemen yang di hapus berada di posisi terakhir array, kembali ke awal array

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -49,6 +49,14 @@ void remove() {
         FRONT = -1;
         REAR = -1;
     }
+
+    else {
+        if (FRONT == max - 1)
+            FRONT = 0;
+        else
+            FRONT = FRONT + 1;
+    }
+
 } 
 
 
