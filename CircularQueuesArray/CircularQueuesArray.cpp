0406b78cca6e8c58e2ff45c11f30aea9ac3f1a4b
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-31 10:43:10 +0700

    add prosedur insert untuk melihat antrian penuh atau tidak

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -12,8 +12,15 @@ public:
         REAR = -1;
     }
 
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
 
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 
-
+        }
+    }
 };
 
