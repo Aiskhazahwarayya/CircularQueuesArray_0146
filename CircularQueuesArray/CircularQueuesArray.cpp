d20593b6b2f0ec4900acda5b137e65bc4b8986b2
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-31 11:35:21 +0700

    iterasi dari awal array hingga REAR

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 class Queues {
 
-	int FRONT, REAR, max = 5;
-	int queue_array[5];
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
 
 public:
     Queues() {
@@ -36,56 +36,65 @@ public:
         }
         queue_array[REAR] = num;
     }
+
+
+    void remove() {
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    void display() {
+
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are...\n";
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+
+        else {
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+            while (FRONT_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
 };
 
-void remove() {
-    if (FRONT == -1) {
-        cout << "Queue underflow\n";
-        return;
-    }
-    cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-
-    if (FRONT == REAR) {
-        FRONT = -1;
-        REAR = -1;
-    }
-
-    else {
-        if (FRONT == max - 1)
-            FRONT = 0;
-        else
-            FRONT = FRONT + 1;
-    }
-} 
-
-void display() {
-
-    int FRONT_position = FRONT;
-    int REAR_position = REAR;
-
-    if (FRONT == -1) {
-        cout << "Queue is empty\n";
-        return;
-    }
-
-    cout << "\nElements in the queue are...\n";
-
-    if (FRONT_position <= REAR_position) {
-        while (FRONT_position <= REAR_position) {
-            cout << queue_array[FRONT_position] << " ";
-            FRONT_position++;
-        }
-        cout << endl;
-    }
-
-    else {
-        while (FRONT_position <= max - 1) {
-            cout << queue_array[FRONT_position] << " ";
-            FRONT_position++;
-        }
-
-        FRONT_position = 0;
-}
 
 
 
